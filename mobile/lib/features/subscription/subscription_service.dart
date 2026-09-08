0d50bdd3a06234d990cd99e458791493e67de793
sufailalayin <78872393+sufailalayin@users.noreply.github.com>
2026-09-08 18:15:06 +0530

    mobile: add subscription service

---
+++ mobile/lib/features/subscription/subscription_service.dart
@@ -0,0 +1,26 @@
+import '../../core/api_client.dart';
+
+class SubscriptionService {
+  SubscriptionService(this._api);
+
+  final ApiClient _api;
+
+  Future<Map<String, dynamic>> status() async {
+    final response = await _api.dio.get('/subscriptions/status');
+    return Map<String, dynamic>.from(response.data as Map);
+  }
+
+  Future<Map<String, dynamic>> verifyGooglePlay({
+    required String productId,
+    required String purchaseToken,
+  }) async {
+    final response = await _api.dio.post(
+      '/subscriptions/google-play/verify',
+      data: {
+        'product_id': productId,
+        'purchase_token': purchaseToken,
+      },
+    );
+    return Map<String, dynamic>.from(response.data as Map);
+  }
+}
