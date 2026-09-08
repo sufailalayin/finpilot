e42d8cc181e795fcea291c6d4408a4ce7c78137d
sufailalayin <78872393+sufailalayin@users.noreply.github.com>
2026-09-08 22:04:42 +0530

    mobile: support default category bootstrap

--- mobile/lib/features/finance/finance_service.dart
+++ mobile/lib/features/finance/finance_service.dart
@@ -15,6 +15,11 @@ class FinanceService {
     return (response.data as List<dynamic>);
   }
 
+  Future<List<dynamic>> bootstrapCategories() async {
+    final response = await _api.dio.post('/finance/categories/bootstrap');
+    return response.data as List<dynamic>;
+  }
+
   Future<List<dynamic>> listTransactions() async {
     final response = await _api.dio.get('/finance/transactions');
     return (response.data as List<dynamic>);
